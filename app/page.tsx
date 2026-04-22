75e4febcda2fcfcc37ad08478a515383a2fb860a
farrelalf <farrelalfach@gmail.com>
2026-04-22 09:51:47 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -138,7 +138,7 @@ export default function HomePage() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "${window.location.origin}/auth/callback",
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     })
   }
